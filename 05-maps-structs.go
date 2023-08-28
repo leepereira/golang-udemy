cde30d2d131cb92f915e048fb1f92596a8bfcc1f
Leon Pereira <7788759+leepereira@users.noreply.github.com>
2023-08-27 22:49:05 -0700

    Update 05-maps-structs.go

--- 05-maps-structs.go
+++ 05-maps-structs.go
@@ -9,13 +9,15 @@ type Bird struct {
 
 func main() {
 
-	pereiraPets := make(map[string]Bird)
+	pereiraPets := make(map[string]Bird) //[string]Bird -->  [key]value
 
+	//creates an instance of Bird
 	pet1 := Bird{
 		Name: "Luna",
 		Age:  1,
 	}
 
+	// instance pet1 is assigned to map pereiraPets using the key "pet1".
 	pereiraPets["pet1"] = pet1
 
 	pet2 := Bird{
@@ -23,9 +25,11 @@ func main() {
 		Age:  1,
 	}
 
+	// instance pet2 is assigned to map pereiraPets using the key "pet2".
 	pereiraPets["pet2"] = pet2
 
-	log.Println("First Pet Name", pet1.Name)
-	log.Println("Second Pet Name", pet2.Name)
+	log.Println("First Pet Name", pereiraPets["pet1"].Name)
+	log.Println("Second Pet Name", pereiraPets["pet2"].Name)
+	log.Println("Second Pet Name", pereiraPets)
 
 }
