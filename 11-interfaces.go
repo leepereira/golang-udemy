3dcd785f4c7c738d62ec1de13df9117d8c65b9ce
Leon Pereira <7788759+leepereira@users.noreply.github.com>
2023-09-02 08:40:21 -0700

    Update 11-interfaces.go

--- 11-interfaces.go
+++ 11-interfaces.go
@@ -1,5 +1,57 @@
 package main
 
-func main() {
+import "fmt"
 
+type Animal interface {
+	Says() string
+	NumberOfLegs() int
+}
+
+type Dog struct {
+	Name  string
+	Breed string
+}
+
+type Gorilla struct {
+	Name          string
+	Color         string
+	NumberOfTeeth int
+}
+
+func main() {
+	dog := Dog{
+		Name:  "Toby",
+		Breed: "Golden Retreiver",
+	}
+
+	PrintInfo(dog)
+
+	gorilla := Gorilla{
+		Name:          "Jock",
+		Color:         "grey",
+		NumberOfTeeth: 32,
+	}
+
+	PrintInfo(gorilla)
+
+}
+
+func PrintInfo(a Animal) {
+	fmt.Println("This animal says", a.Says(), "and has ", a.NumberOfLegs(), "legs")
+}
+
+func (d Dog) Says() string {
+	return "Woof"
+}
+
+func (d Dog) NumberOfLegs() int {
+	return 4
+}
+
+func (g Gorilla) NumberOfLegs() int {
+	return 2
+}
+
+func (g Gorilla) Says() string {
+	return "hoho"
 }
