d4c887ebab151f10c3b3ae5e4f690b4d90987a4f
Leon Pereira <lee_pereira@msn.com>
2023-09-03 22:42:31 -0700

    adding in the html templates

--- 18-http-templates/main.go
+++ 18-http-templates/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
@@ -9,12 +10,22 @@ const portNumber = ":8080"
 
 // Home is the home page handler
 func Home(w http.ResponseWriter, r *http.Request) {
-
+	renderTemplate(w, "home.page.html")
 }
 
 // About is the about page handler
 func About(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "about.page.html")
 
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("error parsing template : ", err)
+		return
+	}
 }
 
 func main() {
